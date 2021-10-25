ed4950fc08f86e6fb5736f13eef8660e2879b7a0
Jeff Phan <jeffphan8816@gmail.com>
2021-10-25 17:55:14 +1100

    Add ErrorModal to valid the input which will show a popup with message. The errorHandler will remove popup when click on Button Okay or at the backdrop

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -3,5 +3,7 @@ import React from "react";
 import classes from './Button.module.css';
 
 const Button = props => {
-    return <button className={classes.button} type={props.type || 'button'} onClick={props.onClick}></button>
+    return <button className={classes.button} type={props.type || 'button'} onClick={props.onClick}>{props.children}</button>
 };
+
+export default Button;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,16 +1,28 @@
-import React, {useState}  from 'react';
+import React, {useState} from 'react';
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css"
+import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
 const AddUser = props => {
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
-    const addUserHandler = (event) =>{
+    const [error, setError] = useState('');
+
+    const addUserHandler = (event) => {
         event.preventDefault();
-        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+        if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({
+                title: "Invalid input",
+                message: "Please enter a valid name (non-empty values)."
+            })
             return;
         }
-        if (+enteredAge < 1){
+        if (+enteredAge < 1) {
+            setError({
+                title: "Invalid input",
+                message: "Please enter a valid age (non-empty values and greater than 0)."
+            })
             return;
         }
         props.onAddUser(enteredUsername, enteredAge);
@@ -18,24 +30,30 @@ const AddUser = props => {
         setEnteredAge('');
     };
 
-    const usernameChangeHandler = (event) =>{
+    const usernameChangeHandler = (event) => {
         setEnteredUsername(event.target.value);
     };
 
-    const ageChangeHandler = (event) =>{
+    const ageChangeHandler = (event) => {
         setEnteredAge(event.target.value);
     };
-    return (
-        <Card className={classes.input}>
-            <form onSubmit={addUserHandler}>
-                <label htmlFor="username">Username</label>
-                <input id={"username"} type={"text"} value={enteredUsername} onChange={usernameChangeHandler}/>
-                <label htmlFor={"age"}>Age(years)</label>
-                <input id={"age"} type={"number"} value={enteredAge} onChange={ageChangeHandler}/>
-                <button type={"submit"}>Add User</button>
-            </form>
-        </Card>
 
+    const errorHandler = () =>{
+        setError(null);
+    };
+    return (
+        <div>
+            {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
+            <Card className={classes.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">Username</label>
+                    <input id={"username"} type={"text"} value={enteredUsername} onChange={usernameChangeHandler}/>
+                    <label htmlFor={"age"}>Age(years)</label>
+                    <input id={"age"} type={"number"} value={enteredAge} onChange={ageChangeHandler}/>
+                    <Button type={"submit"}>Add User</Button>
+                </form>
+            </Card>
+        </div>
     );
 
 };
